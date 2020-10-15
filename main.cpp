120f4d366ed6de123941d4a3ef5fc16eb34f701c
Mike Abbott <furrysalamander@gmail.com>
2020-10-14 20:33:32 -0600

    further adjust debug output

--- main.cpp
+++ main.cpp
@@ -123,6 +123,8 @@ int main()
     {
         read(i2c_fd, &rx_buffer, sizeof(rx_buffer_t)); // Read in the button and joystick status
         cout << '\r';
+        cout << "                                                 ";
+        cout << '\r';
 
         for (int i = 0; i < BUTTON_COUNT - 1; i++)
         {
