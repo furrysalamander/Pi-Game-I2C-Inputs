185d06e6f34d21d252ad7acce8063069af4635f3
Joseph Summerhays <summerhays.joseph@gmail.com>
2021-01-05 18:30:20 -0700

    blah

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ mkdir build
 cd build
 cmake -DST7796S=ON -DGPIO_TFT_DATA_CONTROL=25 -DGPIO_TFT_RESET_PIN=27 -DGPIO_TFT_BACKLIGHT=24 -DSPI_BUS_CLOCK_DIVISOR=4 -DDISPLAY_SWAP_BGR=ON -DBACKLIGHT_CONTROL=ON ..
 make -j
-./fbcp-ili9312 &
+./fbcp-ili9341 &
 
 sed -i "s/exit/~\/fbcp-ili9312 \&\n\~\/Pi-Game-I2C-Inputs\/i2c_driver \&\n&/g" /etc/rc.local
 
